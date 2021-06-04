136aa89fe61137b17ada7737bd6179c6718929b0
mike.benkovich <mike.benkovich@promega.com>
2021-06-04 14:27:32 -0500

    Update code

--- Deploy/test-deploy.ps1
+++ Deploy/test-deploy.ps1
@@ -1,5 +1,5 @@
 cd .\Deploy
 
-az deployment group create --name redis-poc-v01 --resource-group rg-redis-poc --template-file main.bicep
+az deployment group create --name redis-poc-v02 --resource-group rg-redis-poc --template-file main.bicep
 
 func azure functionapp publish bnkredispoc-fn
